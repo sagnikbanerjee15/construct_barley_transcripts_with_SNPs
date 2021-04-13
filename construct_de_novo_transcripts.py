ed81f840daa02f7228147f83bfdc219c895f15e9
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-13 09:53:22 -0500

    Update construct_de_novo_transcripts.py

--- construct_de_novo_transcripts.py
+++ construct_de_novo_transcripts.py
@@ -1,7 +1,38 @@
 
+import sys
+import os
+
+def downloadCI16151data():
+    srr_id_filename = "/work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/raw_data/list_of_ids"
+    cmd  = f" /work/LAS/rpwise-lab/sagnik/Finder/utils/downloadAndDumpFastqFromSRA.py "
+    cmd += f"-s {srr_id_filename}"
+    cmd += f"-n 32"
+    cmd += f"-o /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/raw_data "
+    cmd += f"1> /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/raw_data/download.output "
+    cmd += f"1> /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/raw_data/download.error "
+    os.system(cmd)
+
+def alignReadsToBarleyBlumeria():
+    srr_id_filename = "/work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/raw_data/list_of_ids"
+    srr_ids = open(srr_id_filename,"r").read().split("\n")[:-1]
+    for srr_id in srr_ids:
+        cmd  = "STAR "
+        cmd += " --runThreadN 32 "
+        cmd += " --genomeDir /work/LAS/rpwise-lab/sagnik/data/finder/Hordeum_vulgare/genome/star_index_without_transcriptome "
+        cmd += " --readFilesIn /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/raw_data/"+srr_id+".fastq "
+        cmd += " --alignIntronMin 20  --alignIntronMax 10000 "
+        cmd += " --limitBAMsortRAM 107374182400 "
+        cmd += " --outSAMattributes NH HI AS nM NM MD jM jI XS "
+        cmd += " --genomeLoad LoadAndKeep "
+        cmd += " --outFileNamePrefix /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/STAR_alignments/"+srr_id+"_ "
+        cmd += " > /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/STAR_alignments/"+srr_id+".output "
+        cmd += " > /work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/STAR_alignments/"+srr_id+".error "
+        if os.path.exists("/work/LAS/rpwise-lab/sagnik/RNA-Seq_90_sample/data/STAR_alignments/"+srr_id+"_Aligned.sortedByCoord.bam"):
+            os.system(cmd)
+        
 
 def constructDeNovoTranscript(gene):
-    gff3_filename = "Barley_Morex_V2_gene_annotation_PGSB.all.gff3"
+    gff3_filename = "/work/LAS/rpwise-lab/sagnik/construct_barley_transcripts_with_SNPs/Barley_Morex_V2_gene_annotation_PGSB.all.gff3"
 
 def main():
     horvu_genes = """HORVU.MOREX.r2.4HG0338120
@@ -26,6 +57,10 @@ HORVU.MOREX.r2.7HG0611160
 HORVU.MOREX.r2.5HG0437940
 HORVU.MOREX.r2.1HG0058670"""
     horvu_genes = horvu_genes.split("\n")
+    
+    downloadCI16151data()
+    
+    alignReadsToBarleyBlumeria()
     
     for gene in horvu_genes:
         constructDeNovoTranscript(gene)
