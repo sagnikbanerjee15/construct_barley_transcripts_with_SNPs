43ce5c3c45cf3a91d547f88c6817d482821dc7ac
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-13 16:45:09 -0500

    push

--- construct_de_novo_transcripts.py
+++ construct_de_novo_transcripts.py
@@ -143,6 +143,13 @@ HORVU.MOREX.r2.1HG0058670"""
         cmd+= f"/90daydata/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/contigs/CI16151_merged_{gene}_Aligned.sortedByCoord.out.bam "
     os.system(cmd)
     
+    cmd = "samtools merge -@ 60 /90daydata/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/merged_from_18_samples.bam  "
+    for gene in horvu_genes:
+        cmd += f"> /90daydata/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/CI16151_merged_{gene}.fastq "
+    os.system(cmd)
+    cmd="samtools index -c -@ 60 /90daydata/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/merged_from_18_samples.bam"
+    os.system(cmd)
+    
     cmd=" samtools index -c -@ 60 /90daydata/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/final.bam "
     os.system(cmd)
         
