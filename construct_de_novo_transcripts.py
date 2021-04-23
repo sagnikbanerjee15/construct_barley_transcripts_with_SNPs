95103b47a67e6eb52c25a92ad14530e2fa0367d5
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-23 14:46:34 -0500

    Update construct_de_novo_transcripts.py

--- construct_de_novo_transcripts.py
+++ construct_de_novo_transcripts.py
@@ -29,6 +29,7 @@ def alignReadsToBarley():
         cmd += " 1> /project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+".output "
         cmd += " 2> /project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+".error "
         print(cmd)
+        sys.stdout.flush()
         """if os.path.exists("/project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+"_Aligned.sortedByCoord.out.bam")==False:
             os.system(cmd)"""
         
