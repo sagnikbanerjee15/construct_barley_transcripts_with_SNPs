1b23d2e3c25d32a30a45c13e520859d553989c2f
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-23 18:59:40 -0500

    Update construct_de_novo_transcripts.py

--- construct_de_novo_transcripts.py
+++ construct_de_novo_transcripts.py
@@ -30,11 +30,8 @@ def alignReadsToBarley():
         cmd += " --outFileNamePrefix /project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+"_ "
         cmd += " 1> /project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+".output "
         cmd += " 2> /project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+".error "
-        print(cmd)
-        sys.stdout.flush()
-        os.system(cmd)
-        """if os.path.exists("/project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+"_Aligned.sortedByCoord.out.bam")==False:
-            os.system(cmd)"""
+        if os.path.exists("/project/maizegdb/sagnik/construct_barley_transcripts_with_SNPs/STAR_alignments/"+srr_id+"_Aligned.sortedByCoord.out.bam")==False:
+            os.system(cmd)
         
 
 def constructDeNovoTranscript(gene):
@@ -177,7 +174,7 @@ HORVU.MOREX.r2.1HG0058670"""
     downloadCI16151data()
     
     alignReadsToBarley()
-    return
+    
     mergeAllSamples()
     
     for gene in horvu_genes:
